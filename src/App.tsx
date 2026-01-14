007e7b10580bb8b7fc6b9b2d0c665601eff5d2cc
LUCASGAIATEIXEIRA <lucasgaiatxra@gmail.com>
2026-01-14 19:57:57 -0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,64 @@
+import { useState } from "react";
 
 
 export function App() {
+    const [hide, setHide] = useState(false);
  return(
   <>
   Bucha de canhão
+  {!hide && <p>teste</p>}{/* isso faz com oque retire do html a redenzaçao desse paragrafo, ou pode ser componente tbm para o navegador não precisar carregar e rendenizar o componente ou oque for e não pesar na pagina ter que carregar muita coisa que não esta sendo vista*/}
+  {hide ? <p>Escondido</p> : <p>Mostrado</p>}{/*isso é um if ternario, se hide for true mostra escondido, se for false mostra mostrado, verifica se hide e true ou false, se for true vai apos o (?) se for false vai apos o (:)*/}
+  <Card title="tanto fez" content="content" footer="footer">
+    teste1
+  </Card>
+  <Card title="não sei">
+    teste2
+    
+  </Card>
+  <ButtonSum />
+  <button onClick={() => {setHide(!hide)}}>Esconda Texto</button>
   </>
+
+
  );
 }
+
+
+interface ICardPorps {
+    title: string;
+    content?: string;
+    footer?: string;
+    children: React.ReactNode;//quer fizer que o componente possa receber outros componentes como filhos, como no exemplo acima com o Card. não vai ser mais <Card /> e sim <Card> TESTE </Card>
+    //obs: tem que ser children mesmo, pq é uma palavra reservada do react para esse tipo de situação.
+    //obs2: React.ReactNode é um tipo que representa qualquer coisa que possa ser renderizada pelo React, como elementos JSX, strings, números, arrays ou fragmentos.
+}
+
+const Card = (props: ICardPorps) => {//ou pode ser tbm ({children} : IcardProps) para pegar só o children ou outros parâmetros específicos de IcardProps se quiser um so deles se quiser usar todos esta no codigo sem comentario
+    return(
+        <div style={{border: "1px solid black", padding: "10px", margin: "10px"}}>
+            <span>Title {props.title}</span>
+            <p>Content {props.content}</p>
+            <footer>final {props.footer}</footer>
+            <div>{props.children}</div>
+        </div>
+    )
+}
+
+
+const ButtonSum = () => {
+    const [count, setCount] = useState(0);
+    
+    return(
+        <button onClick={() =>setCount(count + 1)}>
+            Click Aqui {count}
+        </button>
+        
+    )
+}
+
+
+//obs: não pode colocar hooks (useState, useEffect, etc) fora de componentes funcionais ou outros hooks após um if, loop ou função, tem que ser sempre no topo do componente funcional ou de outro hook.
+
+
 
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { App } from './App.tsx'
+// import { App } from './App.tsx'
+import { Parte1 } from "./parte1.tsx"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    {/* <App /> */}
+    <Parte1 />
+  </StrictMode>,//esse strict mode ajuda a encontrar erros no codigo
 )
+
+
+//use state é um hook do react que permite adicionar estado a componentes funcionais. Ele retorna um array com dois elementos: o valor atual do estado e uma função para atualizá-lo.
+//use effect é outro hook do react que permite executar efeitos colaterais em componentes funcionais, como buscar dados, configurar assinaturas ou modificar o DOM. Ele aceita uma função que é executada após a renderização do componente e um array de dependências que determina quando o efeito deve ser reexecutado.
+//use callback é um hook do react que retorna uma versão memoizada de uma função de callback. Ele é útil para evitar recriações desnecessárias de funções em componentes filhos que dependem dessas funções como props.
+//use memo é um hook do react que retorna um valor memoizado. Ele é útil para evitar cálculos caros em cada renderização, armazenando o resultado de uma função e retornando-o novamente se as dependências não mudaram.
+// use effect é um hook do React que permite executar efeitos colaterais em componentes funcionais, como buscar dados, configurar assinaturas ou modificar o DOM. Ele aceita uma função que é executada após a renderização do componente e um array de dependências que determina quando o efeito deve ser reexecutado.
+//use reducer é um hook do React que é usado para gerenciar o estado complexo em componentes funcionais. Ele é uma alternativa ao useState e é inspirado no padrão de design Redux. O useReducer aceita um reductor (uma função que determina como o estado deve ser atualizado com base em uma ação) e um estado inicial, retornando o estado atual e uma função dispatch para enviar ações ao reductor.
+// 
